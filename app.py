899482a6f199c746e7562c9b72eea3973af9be73
pankajinnitk <pankajinnitk@gmail.com>
2017-07-03 00:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -77,7 +77,7 @@ def makeQuery(req):
     elif action == "find-count":               
         return "$count?%24filter=PurchaseOrderLifeCycleStatusCodeText%20eq%20'" + status + "'"
     elif action == "trigger-action":               
-        return "Cancel?ObjectID='00163E0E47D31ED6B18DE0A9A41045DC'"
+        return "Cancel?ObjectID='00163E0E47D31ED797C9DCC7D9BB6D4D'"
     else:
         return {}
 	
