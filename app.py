c8714c1d10c93d9f3ceae28d4cb50d3ecf4e91ea
pankajinnitk <pankajinnitk@gmail.com>
2017-07-02 22:48:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,10 +96,10 @@ def makeWebhookResult(data, req):
         else:
             speech = "There are no purchase orders in the system with " + \
                       req.get("result").get("parameters").get("status") + " status"
-    elif action == "trigger-action":    
-	 speech = "PO cancelled succesfully " 
+    elif action == "trigger-action":
+	speech = "PO cancelled succesfully " 
     else:
-         speech = "Sorry, I did not understand you! Please try again"
+	speech = "Sorry, I did not understand you! Please try again"
 	
     print("Response:")
     print(speech)
